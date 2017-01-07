e3a40fddc961b77e366a3f3f54cc9c44cdb7ad00
Electron <devversitymail@gmail.com>
2017-01-07 23:11:38 +0530

    Added ListenerService in app

---
+++ app/src/main/java/com/example/android/sunshine/SunshineWatchFaceListenerService.java
@@ -0,0 +1,10 @@
+package com.example.android.sunshine;
+
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class SunshineWatchFaceListenerService extends WearableListenerService {
+    public SunshineWatchFaceListenerService() {
+    }
+
+
+}
